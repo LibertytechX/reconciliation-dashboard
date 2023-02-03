dab3d0d319b53aabbc28527b58fb705e1578fa39
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-02-03 14:56:05 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,81 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const column = [
+  { field: 'id', headerName: 'Known ID', width: 90 },
+  { field: 'mandate', headerName: 'Mandate' },
+  { field: 'authcode', headerName: 'Authentication Code', width: 150 },
+  { field: 'phone', headerName: 'Phone', width: 130 },
+  { field: 'tenure', headerName: 'Tenure' },
+  { field: 'amount', headerName: 'Amount' },
+  { field: 'interest', headerName: 'Interest' },
+  { field: 'outstanding', headerName: 'Outstanding' },
+  { field: 'repayment', headerName: 'Repayment' },
+  { field: 'ramount', headerName: 'RRR Amount' },
+  { field: 'Rrepayment', headerName: 'RRR Repayment' },
+  { field: 'status', headerName: 'Status', width: 85 },
+  { field: 'rtenure', headerName: 'RRR Tenure' },
+  { field: 'routstanding', headerName: 'RRR Outstanding' },
+  { field: 'verdict', headerName: 'Verdict', width: 80 },
+];
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['Repodata'],
+    queryFn: async () => {
+      const { data } = await axios.get('http://localhost:3001/data');
+      return data;
+    },
+  });
+
+  const { data: rData } = useQuery({
+    queryKey: ['remData'],
+    queryFn: async () => {
+      const { data } = await axios.get('http://localhost:3003/data');
+      return data;
+    },
+  });
+  // interface tables {
+  //   id: number;
+  //   mandate: string;
+  // }
+
+  const table: any[] = [];
+  data?.forEach((datas: any) => {
+    table.push(datas);
+  });
+  const remData = data?.concat(rData);
+
+  const remId = rData?.map((s: { id: any }) => s.id);
+  const key = data?.map((l: { id: any }) => l.id);
+  const handleClick = () => {
+    if (key !== remId) {
+      return {data}
+    } else {
+      return { remData };
+    }
+  };
+  
+  console.log(remData.length);
+
+  let sum = 0;
+  data?.forEach((num: { amount: number }) => {
+    sum += num.amount;
+  });
+  let pay = 0;
+  data?.forEach((num: { repayment: number }) => {
+    pay += num.repayment;
+  });
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
   return (
     <>
       <Head>
@@ -14,110 +84,45 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+      <main className="h-screen w-full flex flex-col justify-center items-center mt-16">
+        <p className="text-6xl text-blue- my-12">Reconciliation Dashboard</p>
+        <div className="w-[90%] flex flex-row justify-between items-center my-8">
+          <div className="flex gap-4 items-center">
+            <p className="text-center text-2xl">Total Amount</p>
+            <button
+              className="border-2 py-2 px-5 bg-blue-500 text-white text-lg 
+            rounded-md shadow-sm hover:bg-blue-300 hover:shadow-lg"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+              {sum}
+            </button>
+            <p className="text-center text-2xl">Total Repayment</p>
+            <button
+              className="border-2 py-2 px-5 bg-blue-500 text-white text-lg 
+            rounded-md shadow-sm hover:bg-blue-300 hover:shadow-lg"
+            >
+              {pay}
+            </button>
+          </div>
+          <div
+            className="border-2 py-2 px-5 bg-orange-400 text-white text-lg 
+            rounded-md shadow-sm hover:bg-orange-300 hover:shadow-lg"
+            onClick={handleClick}
+          >
+            Load
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <div className="h-screen w-[90%] flex-shrink">
+          <DataGrid
+            rows={data}
+            columns={column}
+            initialState={{
+              pagination: {
+                pageSize: 10,
+              },
+            }}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
-  )
+  );
 }
